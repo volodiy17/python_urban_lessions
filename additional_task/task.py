132f51aa3503cebff111f7aecd708dfd088fe803
Vladimir Bogdanenko <vladimir.bogdanenko@opsguru.ru>
2024-10-10 18:51:30 +0300

    ADD: additional_task tasks

--- additional_task/task.py
+++ additional_task/task.py
@@ -1,8 +1,10 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 
-students = list(students)
-students.sort()
-grades = [sum(grade) / len(grade) for grade in grades]
-results = dict(zip(students, grades))
-print(results)
+# students = list(students)
+# students.sort()
+# grades = [sum(grade) / len(grade) for grade in grades]
+# results = dict(zip(students, [sum(grade) / len(grade) for grade in grades]))
+# print(results)
+
+print(dict(zip(sorted(list(students)), [sum(grade) / len(grade) for grade in grades])))
