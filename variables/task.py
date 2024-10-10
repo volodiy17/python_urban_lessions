bd38c215e3196963e76324142c8eb6ae5fd5c042
Vladimir Bogdanenko <vladimir.bogdanenko@opsguru.ru>
2024-10-10 10:45:25 +0300

    ADD: variables tasks

---
+++ variables/task.py
@@ -0,0 +1,8 @@
+number_of_complited_hw = 12
+number_of_hours_spent = 1.5
+course_name = "Python"
+time_for_one_task = number_of_hours_spent / number_of_complited_hw
+print(f"Курс: {course_name}, "
+      f"всего задач: {number_of_complited_hw}, "
+      f"затрачено часов: {number_of_hours_spent}, "
+      f"среднее время выполнения {time_for_one_task} часа.")
