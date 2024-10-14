3d80abaa87c53763f856473ed44b4997339ff743
Vladimir Bogdanenko <vladimir.bogdanenko@opsguru.ru>
2024-10-14 20:21:48 +0300

    FIX: modules_and_packages tasks

---
+++ modules_and_packages/modules_and_packages/module_4_1.py
@@ -0,0 +1,11 @@
+from fake_math import divide as fake
+from true_math import divide as real
+
+result1 = fake(69, 3)
+result2 = fake(3, 0)
+result3 = real(49, 7)
+result4 = real(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
---
+++ modules_and_packages/modules_and_packages/true_math.py
@@ -0,0 +1,7 @@
+from math import inf
+
+
+def divide(first: int, second: int):
+    if second == 0:
+        return inf
+    return first / second
