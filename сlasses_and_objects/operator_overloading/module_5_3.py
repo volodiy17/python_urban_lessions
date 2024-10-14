c773e3c82a883ebb0eb95c784093edaef15cdf13
Vladimir Bogdanenko <vladimir.bogdanenko@opsguru.ru>
2024-10-14 22:12:04 +0300

    FIX: operator_overloading tasks

---
+++ сlasses_and_objects/operator_overloading/module_5_3.py
@@ -0,0 +1,25 @@
+from house import House
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+print(h1)
+print(h2)
+
+print(h1 == h2) # __eq__
+
+h1 = h1 + 10 # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10 # __iadd__
+print(h1)
+
+h2 = 10 + h2 # __radd__
+print(h2)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
