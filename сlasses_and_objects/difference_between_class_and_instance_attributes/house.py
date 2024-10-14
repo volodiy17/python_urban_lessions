0cc66539a0722b02ab19c48e566dfe4372d17d71
Vladimir Bogdanenko <vladimir.bogdanenko@opsguru.ru>
2024-10-14 23:04:05 +0300

    FIX: difference_between_class_and_instance_attributes tasks

---
+++ сlasses_and_objects/difference_between_class_and_instance_attributes/house.py
@@ -0,0 +1,55 @@
+class House:
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        cls.houses_history.append(args[0])
+        return super().__new__(cls)
+
+    def __init__(self, name: str, number_of_floors: int):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+
+    def __del__(self):
+        print(f"{self.name} снесён, но он останется в истории")
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __eq__(self, other: 'House'):
+        return self.number_of_floors == other.number_of_floors
+
+    def __lt__(self, other: 'House'):
+        return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other: 'House'):
+        return self.number_of_floors <= other.number_of_floors
+
+    def __gt__(self, other: 'House'):
+        return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other: 'House'):
+        return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other: 'House'):
+        return self.number_of_floors != other.number_of_floors
+
+    def __add__(self, value: int):
+        self.number_of_floors += value
+        return self
+
+    def __radd__(self, value: int):
+        return self.__add__(value)
+
+    def __iadd__(self, value: int):
+        return self.__add__(value)
+
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print("Такого этажа не существует")
+            return
+        for i in range(1, new_floor + 1):
+            print(i)
+        return
---
+++ сlasses_and_objects/difference_between_class_and_instance_attributes/module_5_4.py
@@ -0,0 +1,14 @@
+from house import House
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+# Удаление объектов
+del h2
+del h3
+
+print(House.houses_history)
