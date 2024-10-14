db245d76a5dc969e039a815fde96ac02c3216f69
Vladimir Bogdanenko <vladimir.bogdanenko@opsguru.ru>
2024-10-14 21:14:30 +0300

    FIX: special_class_methods tasks

---
+++ сlasses_and_objects/special_class_methods/house.py
@@ -0,0 +1,18 @@
+class House:
+    def __init__(self, name: str, number_of_floors: int):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print("Такого этажа не существует")
+            return
+        for i in range(1, new_floor + 1):
+            print(i)
+        return
---
+++ сlasses_and_objects/special_class_methods/module_5_2.py
@@ -0,0 +1,12 @@
+from house import House
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+# __str__
+print(h1)
+print(h2)
+
+# __len__
+print(len(h1))
+print(len(h2))
