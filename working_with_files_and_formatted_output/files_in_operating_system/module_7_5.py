842f8dd8adafc5aebf63a8cf9458c16e82d8ff87
Vladimir Bogdanenko <vladimir.bogdanenko@opsguru.ru>
2024-12-09 17:19:43 +0300

    ADD: files_in_operating_system tasks

--- working_with_files_and_formatted_output/files_in_operating_system/module_7_5.py
+++ working_with_files_and_formatted_output/files_in_operating_system/module_7_5.py
@@ -5,10 +5,9 @@ directory = "./"
 
 for root, dirs, files in os.walk(directory):
     for file in files:
-        filepath = os.path.join(file)
-        filetime = os.path.getmtime(filepath)
-        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
-        filesize = os.path.getsize(file)
-        parent_dir = os.path.dirname(filepath)
-        print(
-            f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
+        file_path = os.path.join(root, file)
+        modification_time = os.path.getmtime(file_path)
+        formatted_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(modification_time))
+        file_size = os.path.getsize(file_path)
+        parent_directory = os.path.dirname(file_path)
+        print(f'Обнаружен файл: {file}, Путь: {file_path}, Размер: {file_size} байт, Время изменения: {formatted_time}, Родительская директория: {parent_directory}')
