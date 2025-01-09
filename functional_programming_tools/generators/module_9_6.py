b196fee85e1648e66ba21ab0db03706289174059
Vladimir Bogdanenko <vladimir.bogdanenko@opsguru.ru>
2025-01-09 22:18:19 +0300

    ADD: Generators

---
+++ functional_programming_tools/generators/module_9_6.py
@@ -0,0 +1,10 @@
+def all_variants(text):
+    n = len(text)
+    for i in range(n):
+        for j in range(i + 1, n + 1):
+            yield text[i:j]
+
+
+a = all_variants("text")
+for i in a:
+    print(i)
