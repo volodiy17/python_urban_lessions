12123dcd6b900f2dbaf2edace6f5df303fb9b014
Vladimir Bogdanenko <vladimir.bogdanenko@opsguru.ru>
2025-01-09 19:46:54 +0300

    ADD: introduction

---
+++ functional_programming_tools/introduction/module_9_1.py
@@ -0,0 +1,9 @@
+def apply_all_func(int_list: list[int | float], *functions):
+    results = {}
+    for function in functions:
+        results[function.__name__] = function(int_list)
+    return results
+
+
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
