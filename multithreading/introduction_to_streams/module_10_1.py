24faa093ef0d2388a103e47805fce4923eef6afd
Vladimir Bogdanenko <vladimir.bogdanenko@opsguru.ru>
2025-01-18 16:58:48 +0300

    FIX: Introduction to streams

---
+++ multithreading/introduction_to_streams/module_10_1.py
@@ -0,0 +1,40 @@
+import time
+from time import sleep
+from threading import Thread
+
+def write_words(word_count, file_name):
+    with open(file_name, "w") as file:
+        for i in range(1, word_count + 1):
+            file.write(f"Какое-то слово № {i}\n")
+            sleep(0.1)
+    print(f"Завершилась запись в файл {file_name}")
+
+# Измерение времени для последовательного вызова функций
+start_time = time.time()
+
+write_words(10, "example1.txt")
+write_words(30, "example2.txt")
+write_words(200, "example3.txt")
+write_words(100, "example4.txt")
+
+end_time = time.time()
+print(f"Работа функций {end_time - start_time}")
+
+# Измерение времени для многопоточного выполнения
+start_time = time.time()
+
+threads = [
+    Thread(target=write_words, args=(10, "example5.txt")),
+    Thread(target=write_words, args=(30, "example6.txt")),
+    Thread(target=write_words, args=(200, "example7.txt")),
+    Thread(target=write_words, args=(100, "example8.txt")),
+]
+
+for thread in threads:
+    thread.start()
+
+for thread in threads:
+    thread.join()
+
+end_time = time.time()
+print(f"Работа потоков {end_time - start_time}")
