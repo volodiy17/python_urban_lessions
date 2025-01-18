d27004c71f7a430670bf14094b18de30f0a82d4b
Vladimir Bogdanenko <vladimir.bogdanenko@opsguru.ru>
2025-01-18 17:51:33 +0300

    ADD: blocking_and_error_handling

---
+++ multithreading/blocking_and_error_handling/bank.py
@@ -0,0 +1,33 @@
+import threading
+import random
+import time
+
+class Bank:
+    def __init__(self):
+        self.balance = 0
+        self.lock = threading.Lock()
+
+    def deposit(self):
+        for _ in range(100):
+            amount = random.randint(50, 500)
+            self.balance += amount
+            print(f"Пополнение: {amount}. Баланс: {self.balance}")
+
+            if self.balance >= 500 and self.lock.locked():
+                self.lock.release()
+
+            time.sleep(0.001)
+
+    def take(self):
+        for _ in range(100):
+            amount = random.randint(50, 500)
+            print(f"Запрос на {amount}")
+
+            if amount <= self.balance:
+                self.balance -= amount
+                print(f"Снятие: {amount}. Баланс: {self.balance}")
+            else:
+                print("Запрос отклонён, недостаточно средств")
+                self.lock.acquire()
+
+            time.sleep(0.001)
---
+++ multithreading/blocking_and_error_handling/module_10_3.py
@@ -0,0 +1,15 @@
+import threading
+from bank import Bank
+
+bk = Bank()
+
+th1 = threading.Thread(target=Bank.deposit, args=(bk,))
+th2 = threading.Thread(target=Bank.take, args=(bk,))
+
+th1.start()
+th2.start()
+
+th1.join()
+th2.join()
+
+print(f'Итоговый баланс: {bk.balance}')
